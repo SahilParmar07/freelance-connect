0ea74fe55fc3a24c225b8755ce1bdd887087c3e1
SahilParmar07 <sahildparmar007@gmail.com>
2025-06-27 01:08:21 +0530

    routing and initialized redux store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import login from './pages/login'
+import Register from './pages/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      freelance-connect
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={login}/>
+        <Route path="/register" element={Register}/>
+      </Routes>
+    </Router>
   )
 }
 
